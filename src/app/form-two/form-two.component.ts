3ee6c0fda8a37b1130247cdb9ecc0a4fbaeb431b
Robin Burri <robinburri@tutanota.com>
2025-01-14 14:23:11 +0100

    add countries data model

--- src/app/form-two/form-two.component.ts
+++ src/app/form-two/form-two.component.ts
@@ -1,9 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { SelectModule } from 'primeng/select';
+import { City, Country } from '../models/dataModels';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { DatePickerModule } from 'primeng/datepicker';
+import { countries } from '../shared/data/countries';
 
 @Component({
   selector: 'app-form-two',
-  imports: [],
+  imports: [
+    CardModule,
+    CommonModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    SelectModule,
+    InputTextModule,
+    FloatLabelModule,
+    DatePickerModule,
+  ],
   templateUrl: './form-two.component.html',
   styleUrl: './form-two.component.scss',
 })
-export class FormTwoComponent {}
+export class FormTwoComponent {
+  countries: Country[] = countries;
+}
---
+++ src/app/models/dataModels.ts
@@ -0,0 +1,9 @@
+export type City = {
+  name: string;
+  code: string;
+};
+
+export type Country = {
+  name: string;
+  alpha2: string;
+};
