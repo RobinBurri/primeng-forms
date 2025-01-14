c09706864c3a89ba084a45ffd419d2d86a70eb27
Robin Burri <robinburri@tutanota.com>
2025-01-14 15:36:21 +0100

    add form 2

--- src/app/form-two/form-two.component.ts
+++ src/app/form-two/form-two.component.ts
@@ -1,18 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AutoFocusModule } from 'primeng/autofocus';
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { DatePickerModule } from 'primeng/datepicker';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FocusTrapModule } from 'primeng/focustrap';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
-import { SelectModule } from 'primeng/select';
-import { City, Country } from '../models/dataModels';
 import { InputTextModule } from 'primeng/inputtext';
-import { FloatLabelModule } from 'primeng/floatlabel';
-import { DatePickerModule } from 'primeng/datepicker';
+import { SelectModule } from 'primeng/select';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { Country } from '../models/dataModels';
 import { countries } from '../shared/data/countries';
 
 @Component({
   selector: 'app-form-two',
   imports: [
+    ReactiveFormsModule,
     CardModule,
     CommonModule,
     InputGroupModule,
@@ -21,10 +27,31 @@ import { countries } from '../shared/data/countries';
     InputTextModule,
     FloatLabelModule,
     DatePickerModule,
+    SelectButtonModule,
+    FocusTrapModule,
+    AutoFocusModule,
+    ButtonModule,
   ],
   templateUrl: './form-two.component.html',
   styleUrl: './form-two.component.scss',
 })
 export class FormTwoComponent {
+  private fb = inject(FormBuilder);
   countries: Country[] = countries;
+  genderOption: any = [
+    { label: 'Male', value: 'm' },
+    { label: 'Female', value: 'f' },
+  ];
+
+  userForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    country: ['', Validators.required],
+    dateOfBirth: ['', Validators.required],
+    gender: ['', Validators.required],
+  });
+
+  onSubmit() {
+    console.log(this.userForm.value);
+  }
 }
